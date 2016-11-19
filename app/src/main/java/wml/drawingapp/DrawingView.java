95060cc9d17b9cd6ed566f772db77d93444c47c5
William Lu <wml@berkeley.edu>
2016-11-19 13:27:11 -0800

    First commit, running app with black lines drawable

---
+++ app/src/main/java/wml/drawingapp/DrawingView.java
@@ -0,0 +1,109 @@
+package wml.drawingapp;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by wml on 11/19/16.
+ */
+
+public class DrawingView extends View {
+    
+    //drawing path
+    private Path drawPath;
+
+    //defines what to draw
+    private Paint canvasPaint;
+
+    //defines how to draw
+    private Paint drawPaint;
+
+    //initial color
+    private int paintColor = 0xFF660000;
+
+    //canvas - holding pen, holds your drawings
+    //and transfers them to the view
+    private Canvas drawCanvas;
+
+    //canvas bitmap
+    private Bitmap canvasBitmap;
+
+    //brush size
+    private float currentBrushSize, lastBrushSize;
+
+    public DrawingView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+
+    private void init(){
+        currentBrushSize = 5;
+        lastBrushSize = currentBrushSize;
+
+        drawPath = new Path();
+        drawPaint = new Paint();
+        drawPaint.setColor(paintColor);
+        drawPaint.setAntiAlias(true);
+        drawPaint.setStrokeWidth(currentBrushSize);
+        drawPaint.setStyle(Paint.Style.STROKE);
+        drawPaint.setStrokeJoin(Paint.Join.ROUND);
+        drawPaint.setStrokeCap(Paint.Cap.ROUND);
+
+        canvasPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(canvasBitmap, 0 , 0, canvasPaint);
+        canvas.drawPath(drawPath, drawPaint);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        //create canvas of certain device size.
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        //create Bitmap of certain w,h
+        canvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+
+        //apply bitmap to graphic to start drawing.
+        drawCanvas = new Canvas(canvasBitmap);
+    }
+
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float touchX = event.getX();
+        float touchY = event.getY();
+        //respond to down, move and up events
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                drawPath.moveTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                drawPath.lineTo(touchX, touchY);
+                break;
+            case MotionEvent.ACTION_UP:
+                drawPath.lineTo(touchX, touchY);
+                drawCanvas.drawPath(drawPath, drawPaint);
+                drawPath.reset();
+                break;
+            default:
+                return false;
+        }
+        //redraw
+        invalidate();
+        return true;
+    }
+
+
+}
